4790778bec9dedef1cd49abf2d186ab4cfb5cb8c
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-06 12:28:58 +0300

    Delete all boilerplate from CRA

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Calculator />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
