d7d5114a0aff4428f9ad45137abab051a1372914
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-06 12:54:49 +0300

    Solve Linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import './Calculator.css';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
     return (
-      <div className='calculator'>
-        <div className='result'>
+      <div className="calculator">
+        <div className="result">
           <p>0</p>
         </div>
 
-        <span className='clear grey'>AC</span>
-        <span className='grey'>+/-</span>
-        <span className='grey'>%</span>
-        <span className='operator'>÷</span>
+        <span className="clear grey">AC</span>
+        <span className="grey">+/-</span>
+        <span className="grey">%</span>
+        <span className="operator">÷</span>
 
         <span>7</span>
         <span>8</span>
         <span>9</span>
-        <span className='operator'>x</span>
+        <span className="operator">x</span>
 
         <span>4</span>
         <span>5</span>
         <span>6</span>
-        <span className='operator'>-</span>
+        <span className="operator">-</span>
 
         <span>1</span>
         <span>2</span>
         <span>3</span>
-        <span className='operator'>+</span>
+        <span className="operator">+</span>
 
-        <span className='double'>0</span>
+        <span className="double">0</span>
         <span>.</span>
-        <span className='operator'>=</span>
+        <span className="operator">=</span>
       </div>
     );
   }
