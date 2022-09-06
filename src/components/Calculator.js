7c422adac536cf702ad806ce1d4e1d11afec5cb5
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-06 12:44:24 +0300

    Add calculator UI

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './Calculator.css';
 
 class Calculator extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Calculator</h1>
+      <div className='calculator'>
+        <div className='result'>
+          <p></p>
+        </div>
+
+        <span className='clear grey'>AC</span>
+        <span className='grey'>+/-</span>
+        <span className='grey'>%</span>
+        <span className='operator'>÷</span>
+
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span className='operator'>x</span>
+
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span className='operator'>-</span>
+
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span className='operator'>+</span>
+
+        <span className='double'>0</span>
+        <span>.</span>
+        <span className='operator'>=</span>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-import Calculator from './components/Calculator';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>
 );
 
