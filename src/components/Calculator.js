59af259bcdfdc7798a1ad0c8b0c5fff6e56f0d6f
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-06 12:25:50 +0300

    Calculator component Added

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Calculator</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
