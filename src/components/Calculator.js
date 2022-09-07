4382facbd4219b7a86383d3a5e69a27689d81946
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-07 12:35:07 +0300

    Add obj and perform operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import './Calculator.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div className="calculator">
-        <div className="result">
-          <p>0</p>
-        </div>
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-        <span>AC</span>
-        <span>+/-</span>
-        <span>%</span>
-        <span className="operator">÷</span>
+  const updateObject = (e) => {
+    const buttonName = e.target.textContent;
+    const myObj = calculate(state, buttonName);
+    setState(myObj);
+    // console.log(myObj);
+  };
 
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
-        <span className="operator">x</span>
-
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
-        <span className="operator">-</span>
-
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span className="operator">+</span>
-
-        <span className="double">0</span>
-        <span>.</span>
-        <span className="operator">=</span>
-      </div>
-    );
+  const { total, next, operation } = state;
+  const op = operation === '%' ? 'mod' : operation;
+  let result = '';
+  if (total) {
+    result = `${total} ${op || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next} ${op || ''}`;
   }
-}
+
+  return (
+    <div className='calculator'>
+      <div className='result'>
+        <p>{result || 0}</p>
+      </div>
+
+      <span onClick={updateObject}>AC</span>
+      <span onClick={updateObject}>+/-</span>
+      <span onClick={updateObject}>%</span>
+      <span onClick={updateObject} className='operator'>
+        ÷
+      </span>
+
+      <span onClick={updateObject}>7</span>
+      <span onClick={updateObject}>8</span>
+      <span onClick={updateObject}>9</span>
+      <span onClick={updateObject} className='operator'>
+        x
+      </span>
+
+      <span onClick={updateObject}>4</span>
+      <span onClick={updateObject}>5</span>
+      <span onClick={updateObject}>6</span>
+      <span onClick={updateObject} className='operator'>
+        -
+      </span>
+
+      <span onClick={updateObject}>1</span>
+      <span onClick={updateObject}>2</span>
+      <span onClick={updateObject}>3</span>
+      <span onClick={updateObject} className='operator'>
+        +
+      </span>
+
+      <span onClick={updateObject} className='double'>
+        0
+      </span>
+      <span onClick={updateObject}>.</span>
+      <span onClick={updateObject} className='operator'>
+        =
+      </span>
+    </div>
+  );
+};
 
 export default Calculator;
