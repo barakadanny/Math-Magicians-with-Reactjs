c0c17f6eda22bc4b36ea3b7a760bb6a96a371097
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-08 13:25:01 +0300

    Add comment

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,7 @@ import calculate from '../logic/calculate';
 import './Calculator.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
-// eslint-disable-line jsx-a11y/no-static-element-interactions
+// function to calculate the result of the operation
 const Calculator = () => {
   const [state, setState] = useState({
     total: null,
@@ -28,70 +28,70 @@ const Calculator = () => {
   }
 
   return (
-    <div className="calculator">
-      <div className="result">
+    <div className='calculator'>
+      <div className='result'>
         <p>{result || 0}</p>
       </div>
 
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         AC
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         +/-
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         %
       </span>
-      <span onClick={updateObject} role="presentation" className="operator">
+      <span onClick={updateObject} role='presentation' className='operator'>
         ÷
       </span>
 
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         7
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         8
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         9
       </span>
-      <span onClick={updateObject} role="presentation" className="operator">
+      <span onClick={updateObject} role='presentation' className='operator'>
         x
       </span>
 
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         4
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         5
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         6
       </span>
-      <span onClick={updateObject} role="presentation" className="operator">
+      <span onClick={updateObject} role='presentation' className='operator'>
         -
       </span>
 
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         1
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         2
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         3
       </span>
-      <span onClick={updateObject} role="presentation" className="operator">
+      <span onClick={updateObject} role='presentation' className='operator'>
         +
       </span>
 
-      <span onClick={updateObject} role="presentation" className="double">
+      <span onClick={updateObject} role='presentation' className='double'>
         0
       </span>
-      <span onClick={updateObject} role="presentation">
+      <span onClick={updateObject} role='presentation'>
         .
       </span>
-      <span onClick={updateObject} role="presentation" className="operator">
+      <span onClick={updateObject} role='presentation' className='operator'>
         =
       </span>
     </div>
