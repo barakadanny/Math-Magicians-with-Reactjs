bdefdf29e1aca84093edb397b743a4e6d602efdd
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-13 14:13:02 +0300

    Header structure completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>Math Magician</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/calculator'>Calculator</Link>
+          </li>
+          <li>
+            <Link to='/quote'>Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
