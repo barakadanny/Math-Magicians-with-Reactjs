90b4a2d581f4af9efa38e1daa3c43efddee06074
niicode <niidarku66@gmail.com>
2022-09-14 11:09:45 +0000

    Fixed linter errors

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom';
 
 describe('Test Header page', () => {
   it('testing Header page UI rendering', () => {
-    const tree = renderer.create(<Header/>).toJSON();
+    const tree = renderer.create(<Header />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
